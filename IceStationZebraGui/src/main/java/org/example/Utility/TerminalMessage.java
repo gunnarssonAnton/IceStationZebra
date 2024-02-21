300d7b3be064fc34fad144e9e30040970a06c152
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-21 18:18:42 +0100

    Added TerminalMessage for sending colorized messages to the TerminarView

---
+++ IceStationZebraGui/src/main/java/org/example/Utility/TerminalMessage.java
@@ -0,0 +1,12 @@
+package org.example.Utility;
+
+import java.awt.*;
+
+public class TerminalMessage {
+    public final String message;
+    public final Color color;
+    public TerminalMessage(String message, Color color){
+        this.message = message;
+        this.color = color;
+    }
+}
