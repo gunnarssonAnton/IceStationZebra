036252e6eea3dacd8c9fd855ebdab4a4cab90c3f
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-21 14:21:08 +0100

    Added color

--- IceStationZebraGui/src/main/java/org/example/view/TerminalView.java
+++ IceStationZebraGui/src/main/java/org/example/view/TerminalView.java
@@ -14,12 +14,15 @@ public class TerminalView extends JPanel {
         this.setLayout(new BorderLayout());
         this.add(this.outPutField(subject),BorderLayout.CENTER);
         this.setPreferredSize(new Dimension(900,500));
+
     }
 
     private TextArea outPutField(PublishSubject<String> subject){
         TextArea textArea = new TextArea();
         textArea.setEditable(false);
-//        Disposable disposable = subject.subscribe(str -> SwingUtilities.invokeLater(() -> {
+        textArea.setBackground(Color.darkGray);
+        textArea.setForeground(Color.orange);
+        //        Disposable disposable = subject.subscribe(str -> SwingUtilities.invokeLater(() -> {
 //            textArea.append("isz › " + str + "\n");
 //
 //        }),throwable -> {
