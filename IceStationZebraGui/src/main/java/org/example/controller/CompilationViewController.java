d047ab95d1947064a96ab98a3958f4d76c51ffff
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-21 21:22:04 +0100

    clean up

--- IceStationZebraGui/src/main/java/org/example/controller/CompilationViewController.java
+++ IceStationZebraGui/src/main/java/org/example/controller/CompilationViewController.java
@@ -1,22 +1,10 @@
 package org.example.controller;
 
-import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.PublishSubject;
-import org.example.Docker.DockerContainer;
-import org.example.Docker.Dockerfile;
 import org.example.Utility.*;
-import org.example.Utility.Colorize;
 import org.example.Utility.Compilation;
-import org.example.Utility.Generate;
-import org.example.Utility.ProcessHandler;
-import org.example.EditFileWindow;
-import org.example.files.FileIO;
 import org.example.view.CompilationView;
 
-import javax.swing.*;
-import java.awt.*;
 
 public class CompilationViewController {
     CompilationView view = new CompilationView();
