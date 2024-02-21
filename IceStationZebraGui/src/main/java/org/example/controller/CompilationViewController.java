65078a2a5416b8f19f3a77355e4c1d9811da0381
Ruben Madsen <ruben@rubenmadsen.com>
2024-02-21 18:24:02 +0100

    Moved compilation methode into a new class

--- IceStationZebraGui/src/main/java/org/example/controller/CompilationViewController.java
+++ IceStationZebraGui/src/main/java/org/example/controller/CompilationViewController.java
@@ -19,47 +19,11 @@ public class CompilationViewController {
     public CompilationViewController(PublishSubject<TerminalMessage> subject){
         this.subject = subject;
         this.view.setOnClick(e -> {
-            runDocker();
+            new Compilation(subject,"noname").runDocker();
         });
     }
-    public void runDocker() {
-        Dockerfile dockerfile = Dockerfile.getBasic("openjdk:11", "image_" + Generate.generateRandomString(8));
-        FileIO file = new FileIO(FileIO.getApplicationRootPath(), "Dockerfile");
-        file.write(dockerfile.toString());
-        ProcessHandler handler = dockerfile.build(file.getPath().toString());
-        Disposable stdoutDisposable = handler.getStdout().subscribeOn(Schedulers.io()).subscribe(out -> {
-            this.subject.onNext(new TerminalMessage(out,Color.white));
-        }, Throwable::printStackTrace
-        , () -> System.out.println("Image stdout complete"));
-        Disposable stderrDisposable = handler.getStderr().subscribeOn(Schedulers.io()).subscribe(err -> {
-            this.subject.onNext(new TerminalMessage(err, Color.red));
-        }, Throwable::printStackTrace
-                , () -> System.out.println("Image stderr complete"));
-        Disposable completionDisposable = handler.getCompletion().subscribe(
-                () -> {
-                    subject.onNext(new TerminalMessage("Image Process completed successfully",Color.GREEN));
-                    runContainer(dockerfile);
-                },
-                throwable -> subject.onNext(new TerminalMessage("Image Process failed: " + throwable.getMessage(),Color.RED))
-        );
 
-    }
-    public void runContainer(Dockerfile dockerfile){
-        DockerContainer container = DockerContainer.getBasic("container_" + Generate.generateRandomString(8), dockerfile);
-        ProcessHandler containerHandler = container.run(new String[0]);
-        Disposable containerStdoutDisposable = containerHandler.getStdout().subscribeOn(Schedulers.io()).subscribe(out -> {
-            this.subject.onNext(new TerminalMessage(out,Color.lightGray));
 
-        });
-        Disposable containerStderrDisposable = containerHandler.getStderr().subscribeOn(Schedulers.io()).subscribe(err -> {
-            this.subject.onNext(new TerminalMessage(err, Color.red));
-        });
-        Disposable containerCompletionDisposable = containerHandler.getCompletion().subscribe(
-                () -> {
-                    this.subject.onNext(new TerminalMessage("Container completed",Color.green));
-                },throwable -> subject.onNext(new TerminalMessage("Process failed: " + throwable.getMessage(),Color.red))
-        );
-    }
     public CompilationView getView(){
         return this.view;
     }
