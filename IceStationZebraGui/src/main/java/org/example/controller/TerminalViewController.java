b815ca2550574a627786a015e4dd5680def8eb9b
Ruben Madsen <ruben@rubenmadsen.com>
2024-04-30 10:17:03 +0200

    Added throttle to the Terminals' subject

--- IceStationZebraGui/src/main/java/org/example/controller/TerminalViewController.java
+++ IceStationZebraGui/src/main/java/org/example/controller/TerminalViewController.java
@@ -1,16 +1,20 @@
 package org.example.controller;
 
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import org.example.Utility.TerminalMessage;
 import org.example.view.TerminalView;
+
+import java.util.concurrent.TimeUnit;
 
 public class TerminalViewController {
     PublishSubject<TerminalMessage> subject = PublishSubject.create();
     TerminalView view = new TerminalView();
 
     public TerminalViewController(){
-        Disposable disposable = subject.subscribe(tm -> {
+        Disposable disposable = subject
+                .throttleFirst(500, TimeUnit.MILLISECONDS).subscribeOn(Schedulers.computation()).subscribe(tm -> {
             this.view.addLine(tm);
         });
     }
